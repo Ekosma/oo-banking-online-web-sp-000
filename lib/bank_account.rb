814c5be5f2989feab9f45f170f60b194ceb10d4c
Evelyn Kosma <ezranaja@gmail.com>
2021-04-03 00:55:28 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
 class BankAccount
 
+  attr_accessor :name, :balance, :status
+  
+  def initialize(name)
+    @name = name
+    @status = "open"
+    @balance = 1000
+  end
+  
+  def deposit(money)
+    @balance = @balance + money
+  end
+  
+  def display_balance
+    "Your balance is $#{balance}."
+  end
+  
+  def valid?
+    if @balance > 0 && @status == "open"
+      true
+    else
+      false
+    end
+  end
+  
+  def close_account
+    @status = "closed"
+  end
 end
